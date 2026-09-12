adda49281929652dca12f1cc1a783e054754ef8e
Dmitry Nametkin <spynay1@gmail.com>
2026-09-12 22:43:43 +0300

    Создан скелет сайта на Spring Boot с подключением Postgres и Thymeleaf

---
+++ myfunday_application/src/main/java/pro/myfunday/myfunday_application/MyfundayApplication.java
@@ -0,0 +1,13 @@
+package pro.myfunday.myfunday_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfundayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfundayApplication.class, args);
+	}
+
+}
---
+++ myfunday_application/src/test/java/pro/myfunday/myfunday_application/MyfundayApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.myfunday.myfunday_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfundayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
